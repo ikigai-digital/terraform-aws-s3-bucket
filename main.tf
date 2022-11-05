24950fdc1a96a6bd9360c4b10ac6034c47cddf39
fergus.macdermot <Fergus.Macdermot+1@projectolympus.co.za>
2022-11-05 15:39:05 +0900

    PP-14: Release code to JFrog

--- main.tf
+++ main.tf
@@ -21,8 +21,22 @@ resource "aws_s3_bucket_versioning" "versioning" {
   tags = merge(var.tags, var.additional_tags)
 }
 
+resource "aws_kms_key" "key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+  tags                    = merge(var.tags, var.additional_tags)
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
   bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
   tags = merge(var.tags, var.additional_tags)
 }
 
